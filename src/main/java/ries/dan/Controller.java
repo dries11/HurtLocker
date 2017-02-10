f1b916550d0f9c15f10ddda8722d3bacfd372949
dries11 <daniel.ries@ymail.com>
2017-02-10 12:54:34 -0500

    Started working on Controller

--- src/main/java/ries/dan/Controller.java
+++ src/main/java/ries/dan/Controller.java
@@ -4,4 +4,12 @@ package ries.dan;
  * Created by danries on 2/9/17.
  */
 public class Controller {
+
+    JerkSonParser jerkSonParser = new JerkSonParser();
+    GroceryEngine groceryEngine = new GroceryEngine();
+
+    public void run(){
+
+    }
+
 }
--- src/main/java/ries/dan/FormatOutput.java
+++ src/main/java/ries/dan/FormatOutput.java
@@ -8,6 +8,14 @@ import java.util.Formatter;
  */
 public class FormatOutput {
 
-    
+    String breakline = "============";
+
+    public String format(String input){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(input);
+        Formatter formatter = new Formatter(stringBuilder);
+        formatter.format("%-30.30s %-30.30s");
+        return stringBuilder.toString();
+    }
 
 }
